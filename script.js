0780261b292a7b6eae8b610c8837dbe7fad81c1e
Hemyl_qa <hemyl.qa@gmail.com>
2024-10-25 19:50:15 +0300

    test.txt updated

---
+++ script.js
@@ -0,0 +1,18 @@
+let counter = 5;
+
+let num = 25; // Number
+
+let str = 'string_test';
+
+let bool = true;
+
+const obj2 = {
+    country: 'Ukraine',
+    city: 'Dnipro',
+    address: {
+        post_code: '49064',
+        location: 'Mazepa Avenye'
+    }
+}
+
+const arr = [1,2,3,4,5]
